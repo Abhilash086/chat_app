82036acf2811400874fb0ad4ef60f8ba34afe234
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-10-17 23:36:46 +0530

    getUsersForSidebar is implemented to fetch all users from DB and backend is completed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 dotenv.config();
@@ -23,6 +25,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
+app.use('/api/users',userRoutes);
 
 app.listen(PORT,()=>{
     connectToMongoDB();
