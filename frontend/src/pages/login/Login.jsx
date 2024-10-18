fe19cbe61b68a9972b35f49a2a928f225ce168ff
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-10-19 00:07:32 +0530

    Installed react icons package and created Sidebar, SearchInput components

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-        <div className='w-full p-6 rounded-lg shadow-md bg bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40'>
             <h1 className='text-3xl font-semibold text-center text-gray-300'>
                 Login <span className='text-amber-600 font-extrabold'> Chat-App</span>
             </h1>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -4,7 +4,7 @@ import GenderCheckbox from './GenderCheckbox'
 const SignUp = () => {
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40'>
         <h1 className='text-3xl font-semibold text-center text-gray-300'>
           Sign Up <span className='text-amber-600 font-extrabold'> Chat-App</span>
         </h1>
