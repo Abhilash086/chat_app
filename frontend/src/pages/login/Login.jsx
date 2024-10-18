124e3da5a26a9e382c5141a55178d4c7922d18d5
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-10-18 22:43:01 +0530

    Login page implemented

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                Login <span className='text-amber-600 font-extrabold'> Chat-App</span>
+            </h1>
+            <form>
+              <div>
+                <label className='label p-2'>
+                  <span className='text-base label-text font-semibold'>Username</span>
+                </label>
+                <input type="text" placeholder='Enter Your Username' className='w-full input focus:ring-2 focus:ring-amber-600 h-10' />
+              </div>
+              <div>
+                <label className='label p-2'>
+                  <span className='text-base label-text font-semibold'>Password</span>
+                </label>
+                <input type="text" placeholder='Enter Your Password' className='w-full input focus:ring-2 focus:ring-amber-600 h-10' />
+              </div>
+              <a href="#" className='text-sm hover:underline hover:text-amber-600 mt-2 inline-block'>Don't have an account?</a>
+              <div>
+                <button className='btn btn-block btn-sm mt-2'>Login</button>
+              </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
