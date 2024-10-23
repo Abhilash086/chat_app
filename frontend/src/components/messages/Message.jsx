67622fccd6f46d640b32541b43dbf55e71852a22
Abhilash Shetty K <abhilashsettyk@gmail.com>
2024-10-23 13:39:03 +0530

    Display messages and createdAt time implemented in UI

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import useConversation from '../../zustand/useConversation'
+import { useAuthContext } from '../../context/AuthContext'
+import { extractTime } from '../../utils/extractTime';
 
-const Message = () => {
+const Message = ({message}) => {
   const {selectedConversation} = useConversation();
+  const {authUser} = useAuthContext();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-amber-500" : "bg-gray-700";
+  const formattedTime = extractTime(message.createdAt)
+  
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src={selectedConversation.profilePic} alt="" />
+                <img src={profilePic} alt="" />
             </div>
         </div>
-        <div className='chat-bubble text-white bg-gray-700'>Hi what is up</div>
-        <div className='chat-footer text-[#909090]'>12:42</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+        <div className='chat-footer text-[#909090]'>{formattedTime}</div>
     </div>
   )
 }
